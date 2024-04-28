ffa6af3ca0dfc454bec5993f76a677ad4347d7bf
oxcode <mrexcelsam1@live.com>
2024-04-28 01:02:46 +0100

    little fixes 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="flex flex-col items-center justify-center text-center my-12 text-blue-700">
         <h2 class="text-[6em] md:text-[12em]">404</h2>
-        <h1 class="md:leading-5xl md:text-5xl text-2xl font-semibold mb-8">Page Not Found!!!</h1>
+        <h1 class="md:leading-5xl md:text-5xl text-2xl font-semibold mb-8">Page Not Found!!</h1>
         <router-link to="/" class="bg-blue-800 text-white rounded p-2 px-4 mt-8">Go Home</router-link>
     </div>
 </template>
